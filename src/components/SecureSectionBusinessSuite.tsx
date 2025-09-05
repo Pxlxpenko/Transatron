5ab0f8aca4c2cdf28c0c65811451151b5a4c9522
Serhii Pylypenko <serhii.pylypenko@dataart.com>
2025-09-05 15:24:48 +0300

    Comment out unused text and image arrays in SecureSectionBusinessSuite and SecureSectionTransferEdge components for future reference.

--- src/components/SecureSectionBusinessSuite.tsx
+++ src/components/SecureSectionBusinessSuite.tsx
@@ -1,33 +1,33 @@
 import SecureStep from "./SecureStep";
 
 export default function SecureSectionBusinessSuite() {
-  const allTexts = [
-    {
-      text: "Secure",
-    },
-    {
-      text: "Simple",
-    },
-    {
-      text: "Compliant",
-    },
-    {
-      text: (
-        <>
-          Crypto payments
-          <br /> for your business
-        </>
-      ),
-      textClassname: "md:text-[72px]",
-    },
-  ];
+  // const allTexts = [
+  //   {
+  //     text: "Secure",
+  //   },
+  //   {
+  //     text: "Simple",
+  //   },
+  //   {
+  //     text: "Compliant",
+  //   },
+  //   {
+  //     text: (
+  //       <>
+  //         Crypto payments
+  //         <br /> for your business
+  //       </>
+  //     ),
+  //     textClassname: "md:text-[72px]",
+  //   },
+  // ];
 
-  const allCoinImages = [
-    "/coins1bg.svg",
-    "/coins2bg.svg",
-    "/coins3bg.svg",
-    "/coins4bg.svg",
-  ];
+  // const allCoinImages = [
+  //   "/coins1bg.svg",
+  //   "/coins2bg.svg",
+  //   "/coins3bg.svg",
+  //   "/coins4bg.svg",
+  // ];
 
   return (
     <div className="bg-white snap-start">
--- src/components/SecureSectionTransferEdge.tsx
+++ src/components/SecureSectionTransferEdge.tsx
@@ -1,32 +1,32 @@
 import SecureStep from "./SecureStep";
 
 export default function SecureSectionTransferEdge() {
-  const allTexts = [
-    {
-      text: (
-        <>
-          powerful
-          <br /> integration
-        </>
-      ),
-    },
-    {
-      text: (
-        <>
-          for <br /> non-custodial
-        </>
-      ),
-    },
-    {
-      text: (
-        <>
-          wallet <br /> providers
-        </>
-      ),
-    },
-  ];
+  // const allTexts = [
+  //   {
+  //     text: (
+  //       <>
+  //         powerful
+  //         <br /> integration
+  //       </>
+  //     ),
+  //   },
+  //   {
+  //     text: (
+  //       <>
+  //         for <br /> non-custodial
+  //       </>
+  //     ),
+  //   },
+  //   {
+  //     text: (
+  //       <>
+  //         wallet <br /> providers
+  //       </>
+  //     ),
+  //   },
+  // ];
 
-  const allCoinImages = ["/coins1bg.svg", "/coins2bg.svg", "/coins3bg.svg"];
+  // const allCoinImages = ["/coins1bg.svg", "/coins2bg.svg", "/coins3bg.svg"];
 
   return (
     <div className="bg-white snap-start">
